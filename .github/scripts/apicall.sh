af841b89dae36aff955aee2f4cca17a496c17a0d
pratrajesh <66231784+pratrajesh@users.noreply.github.com>
2023-02-06 22:56:11 +0530

    Update apicall.sh

--- .github/scripts/apicall.sh
+++ .github/scripts/apicall.sh
@@ -8,5 +8,7 @@ request_payload={\"body\":\"$(cat error.log)\"}
 curl --silent \
     --request POST \
     --data '$request_payload' \
-    --header  'Authorization: 2f28cdbb-c5df-4146-a8c4-05046a380d00' \
+    -H 'accept: application/json' \
+    -H 'Authorization: 2f28cdbb-c5df-4146-a8c4-05046a380d00' \
+    -H 'Content-Type: application/json' \
     https://api.swaggerhub.com/apis/RAJESHP1231234_1/Rajesh/1.0.0/comments
