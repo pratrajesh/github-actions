d012570002add1315796fe250cb65b7f5a776db3
pratrajesh <66231784+pratrajesh@users.noreply.github.com>
2023-02-07 15:55:44 +0530

    Update apicall.sh

--- .github/scripts/apicall.sh
+++ .github/scripts/apicall.sh
@@ -6,19 +6,16 @@
 #request_payload={\"position\":\"0\" , \"body\":\"$(cat error.log)\"}
 
 #request_payload={\"body\":\"$(cat error.log)\"}
-#request_payload={\"body\":\"$(cat error.log)\",\"position\":0}
+request_payload={\"body\":\"$(cat error.log)\",\"position\":0} >> body.json
 
 #request_payload={\"position\":0,"body":"Rajesh"}
 
 echo ":::::$request_payload"
 
+echo "------$body.json"
+
 curl --location --request POST 'https://api.swaggerhub.com/apis/RAJESHP1231234_1/Rajesh/1.0.0/comments' \
---header 'accept: application/json' \
---header 'Authorization: 2f28cdbb-c5df-4146-a8c4-05046a380d00' \
--header 'Content-Type: application/json; charset=utf-8' \
---data-binary @- << EOF
-{
-    "position": 0,
-    "body": "$(cat error.log)"
-}
-EOF
+-H 'accept: application/json' \
+-H 'Authorization: 2f28cdbb-c5df-4146-a8c4-05046a380d00' \
+-H 'Content-Type: application/json; charset=utf-8' \
+-d @body.json
