51bfbfeb11e7a4a261faa1f83056769bcee63ee9
pratrajesh <66231784+pratrajesh@users.noreply.github.com>
2023-02-09 17:42:02 +0530

    Update apicall.sh

--- .github/scripts/apicall.sh
+++ .github/scripts/apicall.sh
@@ -2,10 +2,9 @@
 #echo "{\"body\":\"$(cat error.log)\",\"position\":0}" > body.json
 
 while read line; do
-  if [ "$line" = '^\s*([0-9]*):([0-9]*)(.*)error(\s*)(.*)' ] 
+  if [ "$line" = '^\s*([0-9]*):([0-9]*)\s*error\s*.*$' ] 
    then echo "$line"
    fi
-  #if [[ $line =~ ^\s*([0-9]*):([0-9]*)(.*)error(\s*)(.*) ]] ; then echo ${BASH_REMATCH[5]}; fi
 done <error.log
 
 lintStatus=$1
